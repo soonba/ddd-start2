676fa55f8efa352f35d6063af6142f062d13f568
sunba <sunba2318@gmail.com>
2022-10-31 11:13:27 +0900

    Money 클래스 생성
    Money 클래스로 해당 멤버변수의 의미를 직관적으로 수정한다.

--- src/main/java/com/myshop/order/Money.java
+++ src/main/java/com/myshop/order/Money.java
@@ -1,9 +1,17 @@
 package com.myshop.order;
 
 public class Money {
-    int won;
+    private final int won;
 
     public Money(int won) {
         this.won = won;
+    }
+
+    public int getWon() {
+        return won;
+    }
+
+    public Money multiply(int quantity) {
+        return new Money(won * quantity);
     }
 }
