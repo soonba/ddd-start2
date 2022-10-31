8b95d264afb4c2b84ab5e78e62cc35db1f90c08b
sunba <sunba2318@gmail.com>
2022-10-31 10:43:04 +0900

    배송정보 refactor

--- src/main/java/com/myshop/order/ShippingInfo.java
+++ src/main/java/com/myshop/order/ShippingInfo.java
@@ -1,37 +1,32 @@
 package com.myshop.order;
 
 public class ShippingInfo {
-    private String receiverName;
-    private String receiverPhoneNumber;
-    private String shippingAddress1;
-    private String shippingAddress2;
-    private String shippingZipcode;
 
-    public ShippingInfo(String receiverName, String receiverPhoneNumber, String shippingAddress1, String shippingAddress2, String shippingZipcode) {
-        this.receiverName = receiverName;
-        this.receiverPhoneNumber = receiverPhoneNumber;
-        this.shippingAddress1 = shippingAddress1;
-        this.shippingAddress2 = shippingAddress2;
-        this.shippingZipcode = shippingZipcode;
+    private Receiver receiver;
+    private Address address;
+
+    public ShippingInfo(Receiver receiver, Address address) {
+        this.receiver = receiver;
+        this.address = address;
     }
 
     public String getReceiverName() {
-        return receiverName;
+        return receiver.getName();
     }
 
     public String getReceiverPhoneNumber() {
-        return receiverPhoneNumber;
+        return receiver.getPhoneNumber();
     }
 
     public String getShippingAddress1() {
-        return shippingAddress1;
+        return address.getAddress1();
     }
 
     public String getShippingAddress2() {
-        return shippingAddress2;
+        return getShippingAddress2();
     }
 
     public String getShippingZipcode() {
-        return shippingZipcode;
+        return address.getZipcode();
     }
 }
